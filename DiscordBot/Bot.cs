8af9985bd79988f52c50c9f1178bddb50681efdd
Robin Grabs <robinmgrabs@gmail.com>
2021-05-20 23:00:56 +0200

    switched to dsharp framework. bot setup plus test commands.

--- DiscordBot/Bot.cs
+++ DiscordBot/Bot.cs
@@ -1,9 +1,11 @@
-﻿using DSharpPlus;
+﻿using DiscordBot.Commands;
+using DSharpPlus;
 using DSharpPlus.CommandsNext;
 using DSharpPlus.EventArgs;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -33,15 +35,24 @@ namespace DiscordBot
                 StringPrefixes = new string[] { Environment.GetEnvironmentVariable("PREFIX") },
                 // Enables users to "@" bot instead of using the assigned prefix
                 EnableMentionPrefix = true,
+                CaseSensitive = false,
+                DmHelp = true,
+                EnableDefaultHelp = true,
             };
 
             Commands = Client.UseCommandsNext(commandsConfig);
+
+            // Add command groups here
+            // Automate this when your not in a bad mood
+            Commands.RegisterCommands<Stress>();
 
             await Client.ConnectAsync();
 
             // Lock method in infinite loop to prevent main from returning and closing
             await Task.Delay(-1);
         }
+
+
 
         private async Task<Task> OnClientReady(DiscordClient client, ReadyEventArgs e)
         {
---
+++ DiscordBot/Commands/Stress.cs
@@ -0,0 +1,31 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordBot.Commands
+{
+    public class Stress : BaseCommandModule
+    {
+        [Command("Hi")]
+        [Description("If no one answers you, I got ya")]
+        public async Task Hi(CommandContext context)
+        {
+            await context.Channel.SendMessageAsync("'Sup?")
+                .ConfigureAwait(false);
+        }
+
+        [Command("Panic")]
+        [Description("Don't panic it's all good, I got you")]
+        public async Task Panic(CommandContext context)
+        {
+            await context.Channel.SendMessageAsync($"Don't worry child, you're {context.Member.DisplayName}, " +
+                $"right now your in {context.Channel.Name}. " +
+                $"In {context.Guild.Owner.DisplayName} safe hands.")
+                .ConfigureAwait(false);
+        }
+    }
+}
